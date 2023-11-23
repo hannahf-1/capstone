886a1c93a048583b237e32b3b66a9028897111a8
emeremikwu <143846761+emeremikwu@users.noreply.github.com>
2023-11-23 11:53:00 -0600

    Update ApplicationDatabase.sql

--- ApplicationDatabase.sql
+++ ApplicationDatabase.sql
@@ -2,65 +2,83 @@ CREATE DATABASE IF NOT EXISTS ApplicationDatabase;
 
 USE ApplicationDatabase;
 
-CREATE TABLE IF NOT EXISTS ApplicationPersonal (
-    ApplicationID INT PRIMARY KEY,
-    LastName VARCHAR (100) NOT NULL,
-    FirstName VARCHAR (100) NOT NULL,
-    MiddleName VARCHAR(100) NOT NULL,
-    StreetAddress VARCHAR(255) NOT NULL,
-    City VARCHAR(100) NOT NULL,
-    State_ VARCHAR(100) NOT NULL,
-    Zip VARCHAR (10) NOT NULL,
-    HomePhone VARCHAR(15) NOT NULL,
-    CellPhone VARCHAR(15) NOT NULL,
-    Email VARCHAR(100) NOT NULL,
-    SocialSecurity VARCHAR(10) NOT NULL,
-    UsCitizen Boolean NOT NULL,
-    ConvictedFelony Boolean NOT NULL,
-    DrugTest Boolean NOT NULL
+-- ApplicationPersonal table
+CREATE TABLE IF NOT EXISTS application_personal (
+    application_id UUID DEFAULT UUID() PRIMARY KEY,
+    last_name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100) NOT NULL,
+    street_address VARCHAR(255) NOT NULL,
+    city VARCHAR(60) NOT NULL,
+    state_ VARCHAR(60) NOT NULL,
+    zip VARCHAR(10) NOT NULL,
+    home_phone VARCHAR(15) NOT NULL,
+    cell_phone VARCHAR(15) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    social_security VARCHAR(12) NOT NULL,
+    us_citizen BOOLEAN NOT NULL,
+    convicted_felony BOOLEAN NOT NULL,
+    drug_test BOOLEAN NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS ApplicationEmployment(
-    ApplicationID INT,
-    Position VARCHAR(100) NOT NULL,
-    DesiredSalary INT NOT NULL,
-    DateAvaliable DATE NOT NULL,
-    Employer VARCHAR(100) NOT NULL,
-    DatesEmployed DATE NOT NULL,
-    WorkPhone VARCHAR(15) NOT NULL,
-    PayRate INT NOT NULL,
-    StreetAddress VARCHAR(255) NOT NULL,
-    City VARCHAR(100) NOT NULL,
-    State_ VARCHAR(100) NOT NULL,
-    Zip VARCHAR(10) NOT NULL,
-    PositionHeld VARCHAR(100) NOT NULL,
-    DutiesPerformed VARCHAR(255) NOT NULL,
-    SupervisorName VARCHAR(255) NOT NULL,
-    ReasonForLeave VARCHAR(255) NOT NULL,
-    CanContact Boolean NOT NULL,
-    FOREIGN KEY (ApplicationID) REFERENCES ApplicationPersonal(ApplicationID)
+-- ApplicationEmploymentHistory table
+CREATE TABLE IF NOT EXISTS application_employment_history (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    application_fk_id UUID,
+    position VARCHAR(100) NOT NULL,
+    desired_salary INT NOT NULL,
+    date_available DATETIME NOT NULL,
+    employer VARCHAR(100) NOT NULL,
+    dates_employed_start DATETIME NOT NULL,
+    dates_employed_end DATETIME NOT NULL,
+    work_phone VARCHAR(16) NOT NULL,
+    pay_rate INT NOT NULL,
+    street_address VARCHAR(255) NOT NULL,
+    city VARCHAR(60) NOT NULL,
+    state_ VARCHAR(60) NOT NULL,
+    zip VARCHAR(10) NOT NULL,
+    position_held VARCHAR(100) NOT NULL,
+    duties_performed VARCHAR(255) NOT NULL,
+    supervisor_name VARCHAR(255) NOT NULL,
+    reason_for_leave VARCHAR(255) NOT NULL,
+    can_contact BOOLEAN NOT NULL,
+    
+    FOREIGN KEY (application_fk_id) REFERENCES application_personal(application_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS ApplicationEducation(
-    ApplcationID INT,
-    SchoolName VARCHAR(255) NOT NULL,
-    YearsAttended INT NOT NULL,
-    SchoolLocation VARCHAR(255) NOT NULL,
-    City VARCHAR(100) NOT NULL,
-    State_ VARCHAR(100) NOT NULL,
-    DegreeRecieved VARCHAR(100) NOT NULL,
-    Major VARCHAR(100) NOT NULL,
-    FOREIGN KEY (ApplicationID) REFERENCES ApplicationPersonal(ApplicationID)
+-- ApplicationEducationHistory table
+CREATE TABLE IF NOT EXISTS application_education_history (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    application_fk_id UUID,
+    school_name VARCHAR(255) NOT NULL,
+    years_attended INT NOT NULL,
+    school_location VARCHAR(255) NOT NULL,
+    city VARCHAR(60) NOT NULL,
+    state_ VARCHAR(60) NOT NULL,
+    degree_received VARCHAR(100) NOT NULL,
+    major VARCHAR(100) NOT NULL,
+    
+    FOREIGN KEY (application_fk_id) REFERENCES application_personal(application_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS ApplicationReference(
-    ApplicationID INT,
-    ReferenceName VARCHAR(100) NOT NULL,
-    Title VARCHAR(100) NOT NULL,
-    Company VARCHAR(100) NOT NULL,
-    PhoneNumber VARCHAR(15) NOT NULL,
-    FOREIGN KEY (ApplicationID) REFERENCES ApplicationPersonal(ApplicationID)
+-- ApplicationReferences table
+CREATE TABLE IF NOT EXISTS application_references (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    application_fk_id UUID,
+    reference_name VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    company VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    
+    FOREIGN KEY (application_fk_id) REFERENCES application_personal(application_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
+
 
 --query to retrieve all application info
 SELECT * FROM ApplicationPersonal;
